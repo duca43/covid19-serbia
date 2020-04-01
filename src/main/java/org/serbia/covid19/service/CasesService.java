e257475609ead04fb3c91a2510cece3cc5d19cba
Nikola Ilic <recimo>
2020-04-01 23:47:37 +0200

    Test scrape

--- src/main/java/org/serbia/covid19/service/CasesService.java
+++ src/main/java/org/serbia/covid19/service/CasesService.java
@@ -48,7 +48,7 @@ public class CasesService {
                 .collect(Collectors.toList());
     }
 
-    @Scheduled(cron = "0 2,5,15,20,46 7,17,23 * * *")
+    @Scheduled(cron = "0 2,5,15,20,49 7,17,21 * * *")
     @EventListener(ApplicationReadyEvent.class)
     public void scrapeCases() {
         final WebClient client = new WebClient();
